e29f2a423c608a4af8234d45671312eb870ba5b5
Antonio V <antoniovlx@gmail.com>
2023-04-18 11:09:05 +0200

    types file extended

--- index.d.ts
+++ index.d.ts
@@ -3,10 +3,26 @@
 import * as L from 'leaflet';
 
 declare module 'leaflet' {
-    namespace Geoserver {
-      export function wms(baseLayerUrl: any, options?: any): any;
-      export function wfs(baseLayerUrl: any, options?: any): any;
-      export function legend(baseLayerUrl: any, options?: any): any;
-      export function wmsImage(baseLayerUrl: any, options?: any): any;
-    }
+  interface GeoserverOptions {
+    layers?: string;
+    format?: string;
+    transparent?: boolean;
+    CQL_FILTER?: string;
+    zIndex?: number;
+    version?: string;
+    srsname?: string;
+    attribution?: string;
+    fitLayer?: boolean;
+    style?: string;
+    onEachFeature?: (feature: any, layer?: Layer) => void;
+    width?: number;
+    height?: number;
+  }
+  
+  namespace Geoserver {
+    export function wms(baseLayerUrl: string, options?: GeoserverOptions): L.TileLayer.WMS;
+    export function wfs(baseLayerUrl: string, options?: GeoserverOptions): any;
+    export function legend(baseLayerUrl: string, options?: GeoserverOptions): L.Control;
+    export function wmsImage(baseLayerUrl: string, options?: GeoserverOptions): any;
+  }
 }
